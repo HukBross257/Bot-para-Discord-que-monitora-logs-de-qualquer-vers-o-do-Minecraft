0bc53c791dbae425df0540e020cc0c6e64b3623d
HukBross <140917589+HukBross257@users.noreply.github.com>
2025-11-06 22:13:24 -0300

    Update blocklogger.py

--- blocklogger.py
+++ blocklogger.py
@@ -10,18 +10,19 @@ def abrir_os_logs():
 
     return open(server_log, 'r')
 
-def mostrar_logs(dados): 
-    ''' Aqui ira pegar os dados do logs e enviar para o canal do discord'''
-
-    dados.seek(0, 2)  # Vai para o final do arquivo
-    discord_weebhook = '' 
-
-    while True:
-        linhas = dados.readlines()
-        linhas = [linha.strip() for linha in linhas]
-
-        for linha in linhas:
-            requests.post(discord_webhook, json={'content': linha})
-            print(linha)
-
-        time.sleep(0.2)
+def mostrar_logs(dados):
+    try:
+        ''' Aqui ira pegar os dados do logs e enviar para o canal do discord'''
+        dados.seek(0, 2)  # Vai para o final do arquivo
+        discord_weebhook = '' 
+        
+        while True:
+            linhas = dados.readlines()
+            linhas = [linha.strip() for linha in linhas]
+            
+            for linha in linhas:
+                requests.post(discord_webhook, json={'content': linha})
+                print(linha)
+            time.sleep(0.2)
+    except Exception as e:
+        print(f'Aconteceu um erro e nao foi possivel enviar para o Discord {e}')
